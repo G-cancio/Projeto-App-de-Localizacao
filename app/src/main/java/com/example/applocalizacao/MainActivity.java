cc7b13c61ee17dc6c396a474139cadaf9fab9cdc
GABRIEL CANCIO SIMPLICIO DOS SANTOS <200035292@UCSAL.BR>
2026-05-06 09:18:13 -0300

    adicionando feature de mapa do google

--- app/src/main/java/com/example/applocalizacao/MainActivity.java
+++ app/src/main/java/com/example/applocalizacao/MainActivity.java
@@ -1,15 +1,37 @@
 package com.example.applocalizacao;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent i = new Intent(this, LocationActivity.class);
-        startActivity(i);
+
+        Button buttonGps = findViewById(R.id.button_gps);
+        Button buttonMap = findViewById(R.id.button_map);
+
+        buttonGps.setOnClickListener(this);
+        buttonMap.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_gps) {
+            Intent i = new Intent(this, LocationActivity.class);
+            startActivity(i);
+        }
+
+        if (view.getId() == R.id.button_map) {
+            Intent i = new Intent(this, MapsActivity.class);
+            startActivity(i);
+        }
     }
 }
